7393ee7802f757b405be2e63dd48b2ba4df53716
fanimator12 <torok.franciska.leonora@gmail.com>
2023-05-02 14:47:00 +0200

    add Start Menu UI

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+    
+}
